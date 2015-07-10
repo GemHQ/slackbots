31514328017e042986a7dc5795031cb6abd77396
Matt Smith <matt.the.smith@gmail.com>
2015-07-10 09:43:47 -0700

    Ringo Von BitBot - he polls blockr for data on a transaction or address and spits updates into slack - super old, may not even function these days.

---
+++ ringo/config.py
@@ -0,0 +1,22 @@
+
+
+API_KEY='FILLMEIN'
+
+
+USERS = {'memmaniac': 'U03RJDUUT',
+         'James': 'U035KQ55W',
+         'elitistbot': 'U03R6U9J7'}
+
+CONFS = ['You got it.',
+         'Sure thing, broheim.',
+         'Totally agree - on it.',
+         'Yeah, okay.',
+         'If you say so.',
+         'Fiiiiiiine.',
+         'Really? If you\'re sure.']
+
+ATTITUDES = {'amicable': CONFS[0:3],
+             'indifferent': CONFS[3:5],
+             'sassy': CONFS[5:]}
+
+USAGE = "Usage: adhocbot [info, watch, unwatch] [btc,tbtc:]txhash -- or did you want me to say something clever?"
